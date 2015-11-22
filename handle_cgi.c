adada1cf158024fc3a26c14eeba20f02c51d3cc8
yanguango <yanguango@gmail.com>
2015-11-22 17:39:12 -0500

    Get CGI path with cgi dir

--- handle_cgi.c
+++ handle_cgi.c
@@ -1,10 +1,19 @@
 #include "handle_cgi.h"
 
+void cgi_respond_404(_response *);
 int cgi_respond(_response *, char *, int);
 
 int
-handle_cgi(/*Input*/const _request *request, /*Output*/_response *response)
+handle_cgi(/*Input*/const _request *req, /*Output*/_response *resp)
 {
+    char *cgipath = get_absolute_path(req->uri, REQ_CGI);
+
+    if (access(cgipath, X_OK) == -1) {
+        perror("access");
+        cgi_respond_404(resp);
+        exit(1);
+    }
+
     int pipefd[2];
     if (pipe(pipefd) == -1) {
         perror("pipe");
@@ -17,7 +26,7 @@ handle_cgi(/*Input*/const _request *request, /*Output*/_response *response)
     } else if (pid == 0) {
         while ((dup2(pipefd[1], STDOUT_FILENO) == -1) && (errno == EINTR)) {}
         close(pipefd[0]);
-        execl(request->uri, (char*)0);
+        execl(req->uri, (char*)0);
         perror("execl");
         _exit(1);
     } else {
@@ -35,7 +44,7 @@ handle_cgi(/*Input*/const _request *request, /*Output*/_response *response)
             } else if (count == 0) {
                 break;
             } else {
-                cgi_respond(response, buffer, count);
+                cgi_respond(resp, buffer, count);
             }
         }
         close(pipefd[0]);
@@ -44,8 +53,15 @@ handle_cgi(/*Input*/const _request *request, /*Output*/_response *response)
     return 0;
 }
 
+void
+cgi_respond_404(_response *resp)
+{
+    resp->code = 404;
+    resp->desc = "Not Found";
+}
+
 int
-cgi_respond(_response *response, char *buffer, int count)
+cgi_respond(_response *resp, char *buffer, int count)
 {
     return 0;
 }
