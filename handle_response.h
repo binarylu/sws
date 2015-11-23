3ea6633e9e8f56203d2e6a23ec936158befc1a72
Lin Liu <lliu19@stevens.edu>
2015-11-22 22:07:24 -0500

    modified:   handle_response.c modified:   handle_response.h modified:   handle_static.c modified:   handle_static.h add the generate_desc in handle_response.c update the head file in handle_static.h

--- handle_response.h
+++ handle_response.h
@@ -5,6 +5,7 @@
 
 void respond_not_found(_response *response); /* 404 */
 void respond_forbidden(_response *response); /* 403 */
+int generate_desc(_response* response)
 
 
 #endif /* end of include guard: __HANDLE_RESPONSE_H__ */
--- handle_static.h
+++ handle_static.h
@@ -3,10 +3,22 @@
 
 #include "public.h"
 
+#define MAX_TIME_SIZE 100
+#define SERVER_NAME "sws"
+#define SERVER_NAME_SIZE 3
+#define VERSION "HTTP/1.0"
+#define BUFF_SIZE 1024
+
+
 /*
  * Needn't allocate/free memory.
  */
 int handle_static(/*Input*/const _request *request, /*Output*/_response *response);
+int if_modified(const _request *request, const struct stat req_stat)
+int same_time(const char* val, const time_t mtime)
+int set_file(const _request *request, const struct stat req_stat, _response *response)
+int set_directory(_request *request, struct stat req_stat, _response *response)
+
 
 #endif /* end of include guard: __HANDLE_STATIC_H__ */
 
