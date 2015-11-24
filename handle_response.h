16ce01d8300a0decf140647d800b0f6567c9eb68
Guang Yang <gyang4@stevens.edu>
2015-11-24 11:38:56 -0500

    Fix syntax error of handle_response.h

--- handle_response.h
+++ handle_response.h
@@ -5,6 +5,6 @@
 
 void respond_not_found(_response *response); /* 404 */
 void respond_forbidden(_response *response); /* 403 */
-int generate_desc(_response* response)
+int generate_desc(_response* response);
 
 #endif /* end of include guard: __HANDLE_RESPONSE_H__ */
