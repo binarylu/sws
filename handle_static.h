655223cd9354fd3caad82c7551d24508432325a4
Guang Yang <gyang4@stevens.edu>
2015-11-24 11:32:48 -0500

    Fix handle_static.h

--- handle_static.h
+++ handle_static.h
@@ -14,10 +14,10 @@
  * Needn't allocate/free memory.
  */
 int handle_static(/*Input*/const _request *request, /*Output*/_response *response);
-int if_modified(const _request *request, const struct stat req_stat)
-int same_time(const char* val, const time_t mtime)
-int set_file(const _request *request, const struct stat req_stat, _response *response)
-int set_directory(_request *request, struct stat req_stat, _response *response)
+int if_modified(const _request *request, const struct stat req_stat);
+int same_time(const char* val, const time_t mtime);
+int set_file(const _request *request, const struct stat req_stat, _response *response);
+int set_directory(_request *request, struct stat req_stat, _response *response);
 
 
 #endif /* end of include guard: __HANDLE_STATIC_H__ */
