3303b5cd0efd4b77ff3a3bd7e0547ec03e582ecc
yanguango <yanguango@gmail.com>
2015-11-24 21:12:37 -0500

    Test request with request txt

--- handle_response.c
+++ handle_response.c
@@ -18,56 +18,56 @@ int
 generate_desc(_response* response)
 {
     switch(response->code) {
-        case 200: 
+        case 200:
             response->desc = "OK";
-            break; 
-        case 201: 
+            break;
+        case 201:
             response->desc = "Created";
-            break; 
-        case 202: 
+            break;
+        case 202:
             response->desc = "Accepted";
-            break; 
-        case 204: 
+            break;
+        case 204:
             response->desc = "No Content";
-            break; 
-        case 300: 
+            break;
+        case 300:
             response->desc = "Multiple Choices";
-            break; 
-        case 301: 
+            break;
+        case 301:
             response->desc = "Moved Permanently";
-            break; 
-        case 302: 
+            break;
+        case 302:
             response->desc = "Moved Temporarily";
-            break; 
-        case 304: 
+            break;
+        case 304:
             response->desc = "Not Modified";
-            break; 
-        case 400: 
+            break;
+        case 400:
             response->desc = "Bad Request";
-            break; 
-        case 401: 
+            break;
+        case 401:
             response->desc = "Unauthorized";
-            break; 
-        case 403: 
+            break;
+        case 403:
             response->desc = "Forbidden";
-            break; 
-        case 404: 
+            break;
+        case 404:
             response->desc = "Not Found";
-            break; 
-        case 500: 
+            break;
+        case 500:
             response->desc = "Internal Server Error";
-            break; 
-        case 501: 
+            break;
+        case 501:
             response->desc = "Not Implemented";
-            break; 
-        case 502: 
+            break;
+        case 502:
             response->desc = "Bad Gateway";
-            break; 
-        case 503: 
+            break;
+        case 503:
             response->desc = "Service Unavailable";
-            break; 
+            break;
         default:
             return -1;
-    } 
+    }
     return 0;
 }
