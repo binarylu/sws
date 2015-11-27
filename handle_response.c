6193fe08b4b21f07462f121d010b86d37685fa38
Lin Liu <lliu19@stevens.edu>
2015-11-27 12:14:53 -0500

    Fix the string bugs

--- handle_response.c
+++ handle_response.c
@@ -4,14 +4,14 @@ void
 respond_not_found(_response *resp)
 {
     resp->code = 404;
-    resp->desc = "Not Found";
+    resp->desc = generate_str("Not Found");
 }
 
 void
 respond_forbidden(_response *resp)
 {
     resp->code = 403;
-    resp->desc = "Forbidden";
+    resp->desc = generate_str("Forbidden");
 }
 
 int
