ca19c2b8c30b22f91545df7d9cca43e7ff31663d
Xin Li <xli76@stevens.edu>
2015-12-02 14:48:57 -0500

    index: enlarge index page buffer

--- index.h
+++ index.h
@@ -1,7 +1,7 @@
 #ifndef _INDEX_H_
 #define _INDEX_H_
 
-#define INDEX_BUFFER_SIZE 4096
+#define INDEX_BUFFER_SIZE (8 * 4096)
 
 /* generate a html page of directory index */
 char * generate_index(const char *path);
